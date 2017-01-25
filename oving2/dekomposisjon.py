fe0195d27da396f10a8e439fc92406ac7d3c175d
Lars Sydnes <sydlar@westerdals.no>
2017-01-25 16:01:28 +0100

    Rettet en skrivefeil

--- oving2/dekomposisjon.py
+++ oving2/dekomposisjon.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 #
+# Skjelettkode for oppgave 2 d)
 #
 
 import math
 
 
+# Kan komme til nytte?
 def dot(u,v):
     return 0
 
+# Dette handler oppgaven om 
 def dekomposisjon(u,v):
 
     return None,None
@@ -18,7 +21,7 @@ def dekomposisjon(u,v):
 u = [3,4,3]
 v = [4,3,2]
 
-vParallell, vPerp = dekomp(u,v)
+vParallell, vPerp = dekomposisjon(u,v)
 
 
 print(vParallell,vPerp)
