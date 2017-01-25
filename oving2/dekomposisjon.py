ceca8648280d2dd8792f62e228d5dd5173196679
Lars Sydnes <sydlar@westerdals.no>
2017-01-25 15:59:44 +0100

    La til skjelettkode for øving2

---
+++ oving2/dekomposisjon.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+#
+#
+
+import math
+
+
+def dot(u,v):
+    return 0
+
+def dekomposisjon(u,v):
+
+    return None,None
+
+
+
+
+u = [3,4,3]
+v = [4,3,2]
+
+vParallell, vPerp = dekomp(u,v)
+
+
+print(vParallell,vPerp)
